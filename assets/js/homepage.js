20c1735e5998f10309e18da678b7635179aaab35
mendezhector16 <mendezhector16@gmail.com>
2022-06-13 15:53:04 -0500

    added error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,9 +10,18 @@ var getUserRepos = function(user) {
 
     //make a request to the url
     fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
-        });
+        // check if request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data,user);
+            });
+        } else {
+            alert("Error: Github User Not Found");
+        }
+    })
+    .catch(function(error) {
+        //notice this '.catch()' getting chained onto the end of the '.then()' method
+        alert("Unable to connect to Github");
     });
 };
 
@@ -40,6 +49,12 @@ var displayRepos = function(repos, searchTerm) {
     //clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
+
+    //check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found."
+        return;
+    }
 
     //loop over repos
     for (var i = 0; i < repos.length; i++) {
