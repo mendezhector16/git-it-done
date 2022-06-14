cc794a58bb61f2eb5d3cef78a3c77ee1b7284e30
mendezhector16 <mendezhector16@gmail.com>
2022-06-14 18:36:27 -0500

    error handling and redirecting to homepage

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,24 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning")
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    //grab repo name from url query string
+    var queryString = document.location.search;
+
+    var repoName = queryString.split("=")[1];
+    
+    //checks if it is a valid repo, if not it redirects it back to index.html
+    if (repoName) {
+        //display reponame on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        //if no repo was given, redirects to homepage
+        document.location.replace("./index.html");
+    }
+
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -18,7 +37,8 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html")
         }
     });
 };
@@ -74,4 +94,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
